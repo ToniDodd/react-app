054186671659590870d3806f230b4d4c960ead45
ToniDodd <121724318+ToniDodd@users.noreply.github.com>
2023-02-03 13:19:36 -0800

    created new forecast day component and added api call for accurate data

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,66 +1,35 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
+import ForecastDay from "./ForecastDay";
 
 
 export default function Forecast(props){
+let [loaded, setLoaded] = useState(false);
+let [forecast, setForecast] = useState(null);
 
 function showForecast(response){
-    console.log(response.data)
+    setForecast(response.data.daily);
+    setLoaded(true);
 }
-
-console.log(props)
-
-
-    let latitude = props.coordinates.latitude
+if(loaded){
+console.log(forecast);
+    return(
+        <div className="row">
+                <div className="col-2">
+                  <ForecastDay data={forecast[0]} />
+                </div>
+                
+                
+         </div>
+            
+    );
+} else{ 
+    
+        let latitude = props.coordinates.latitude
     let longitude = props.coordinates.longitude
     let apiUrl=`https://api.shecodes.io/weather/v1/forecast?lat=${latitude}&lon=${longitude}&key=tb9021cb57677162636fa4a00f5o70a3&units=imperial`
     
     axios.get(apiUrl).then(showForecast);
-    return(
-        <div className="row">
-                <div className="col-2">
-                  <ul>
-                    <li>Thu</li>
-                    <li><img src="https://ssl.gstatic.com/onebox/weather/48/rain_light.png" alt="rain" /></li>
-                    <li>45</li>
-                  </ul>
-                </div>
-                <div className="col-2">
-                  <ul>
-                    <li>Fri</li>
-                    <li><img src="/"/></li>
-                    <li>40</li>
-                  </ul>
-                </div>
-                <div className="col-2">
-                  <ul>
-                    <li>Sat</li>
-                    <li><img src="https://ssl.gstatic.com/onebox/weather/48/rain_light.png" alt="rain" /></li>
-                    <li>43</li>
-                  </ul>
-                </div>
-                <div className="col-2">
-                  <ul>
-                    <li>Sun</li>
-                    <li><img src="https://ssl.gstatic.com/onebox/weather/48/rain_light.png" alt="rain" /></li>
-                    <li>43</li>
-                  </ul>
-                </div>
-                <div className="col-2">
-                  <ul>
-                    <li>Mon</li>
-                    <li><img src="https://ssl.gstatic.com/onebox/weather/48/rain_light.png" alt="rain" /></li>
-                    <li>48</li>
-                  </ul>
-                </div>
-                <div className="col-2">
-                  <ul>
-                    <li>Tue</li>
-                    <li><img src="https://ssl.gstatic.com/onebox/weather/48/rain_light.png" alt="rain" /></li>
-                    <li>43</li>
-                  </ul>
-                </div>
-              </div>
-            
-    )
+ return null;
+}
 }
---
+++ src/ForecastDay.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+export default function ForecastDay(props){
+
+    function day(){
+        let date = new Date (props.data.time * 1000);
+        let day = date.getDay();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
+        return days[day];
+    }
+    return(
+        
+            <ul>
+                <li>{day()}</li>
+                <li><img src={props.data.condition.icon_url} alt={props.data.condition.icon} /></li>
+                <li>{Math.round(props.data.temperature.maximum)}°<span>{Math.round(props.data.temperature.minimum)}°</span></li>
+            </ul>
+        
+    )
+
+}
