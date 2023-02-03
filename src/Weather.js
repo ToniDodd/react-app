52c450a033d9be562ca78ecee32d80f6a3409962
ToniDodd <121724318+ToniDodd@users.noreply.github.com>
2023-02-02 20:16:36 -0800

    added api call to search

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { Dna } from  'react-loader-spinner'
 import React, { useState } from "react";
-import DateFormat from "./DateFormat";
+
+import WeatherInfo from "./WeatherInfo";
 
 export default function Weather(props) {
     
 const [weatherData, setWeatherData] = useState({ready: false});
+const [city, setCity] = useState(props.defaultCity)
 
 function showData(response){
     console.log(response)
@@ -25,92 +27,42 @@ function showData(response){
     });
 }
 
+function search(){
+    
+    let apiUrl= `https://api.shecodes.io/weather/v1/current?query=${city}&key=tb9021cb57677162636fa4a00f5o70a3&units=imperial`;
+axios.get(apiUrl).then(showData)
+
+}
+
+function citySubmit(event){
+    event.preventDefault()
+    search();
+}
+
+function citySearch(event){
+    setCity(event.target.value);
+
+}
+
 if (weatherData.ready){
     return (
 <div className="weather-app">
-        <form>
+        <form onSubmit={citySubmit}>
             <div className="row">
                 <div className="col-9">
-          <input type="search" placeholder="Enter a city..." className="city-search" /></div>
+          <input type="search" placeholder="Enter a city..." className="city-search" autofocus="on" onChange={citySearch} /></div>
           <div className="col-3">
           <input type="submit" value="Search" className="city-submit" />
           </div>
           </div>
-          <h2 className="city">{weatherData.city}</h2>
-          <div classname="container">
-            <div className="row">
-              <div className="col-6">
-                <ul>
-                  <li><DateFormat date={weatherData.date} /></li>
-                  <li>{weatherData.description}</li>
-                  <li><img src={weatherData.icon} alt={weatherData.iconDescription} />
-                  <span className="temperature">{weatherData.temperature}</span>°<span>F
-                    </span></li>
-                </ul>
-              </div>
-              <div className="col-6">
-                <ul>
-                  <li>Humidity: {weatherData.humidity}%</li>
-                  <li>Wind: {weatherData.wind}mph</li>
-                </ul>
-              </div>
-              <hr />
-              <div className="row">
-                <div className="col-2">
-                  <ul>
-                    <li>Thu</li>
-                    <li><img src="https://ssl.gstatic.com/onebox/weather/48/rain_light.png" alt="rain" /></li>
-                    <li>45</li>
-                  </ul>
-                </div>
-                <div className="col-2">
-                  <ul>
-                    <li>Fri</li>
-                    <li><img src="https://ssl.gstatic.com/onebox/weather/48/rain_light.png" alt="rain" /></li>
-                    <li>40</li>
-                  </ul>
-                </div>
-                <div className="col-2">
-                  <ul>
-                    <li>Sat</li>
-                    <li><img src="https://ssl.gstatic.com/onebox/weather/48/rain_light.png" alt="rain" /></li>
-                    <li>43</li>
-                  </ul>
-                </div>
-                <div className="col-2">
-                  <ul>
-                    <li>Sun</li>
-                    <li><img src="https://ssl.gstatic.com/onebox/weather/48/rain_light.png" alt="rain" /></li>
-                    <li>43</li>
-                  </ul>
-                </div>
-                <div className="col-2">
-                  <ul>
-                    <li>Mon</li>
-                    <li><img src="https://ssl.gstatic.com/onebox/weather/48/rain_light.png" alt="rain" /></li>
-                    <li>48</li>
-                  </ul>
-                </div>
-                <div className="col-2">
-                  <ul>
-                    <li>Tue</li>
-                    <li><img src="https://ssl.gstatic.com/onebox/weather/48/rain_light.png" alt="rain" /></li>
-                    <li>43</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </form>
-      
+          </form>
+          <WeatherInfo data={weatherData}/>
+         
  
     </div>
   ); 
 } else{
-    
-    let apiUrl= `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=tb9021cb57677162636fa4a00f5o70a3&units=imperial`;
-axios.get(apiUrl).then(showData)
-
+    search();
 return(<Dna
     visible={true}
     height="80"
---
+++ src/WeatherInfo.js
@@ -0,0 +1,76 @@
+import React from "react";
+import DateFormat from "./DateFormat";
+
+export default function WeatherInfo(props){
+    return(
+        <div classNmae="WeatherInfo">
+        <h2 className="city">{props.data.city}</h2>
+          <div classname="container">
+            <div className="row">
+              <div className="col-6">
+                <ul>
+                  <li><DateFormat date={props.data.date} /></li>
+                  <li>{props.data.description}</li>
+                  <li><img src={props.data.icon} alt={props.data.iconDescription} />
+                  <span className="temperature">{props.data.temperature}</span>°<span>F
+                    </span></li>
+                </ul>
+              </div>
+              <div className="col-6">
+                <ul>
+                  <li>Humidity: {props.data.humidity}%</li>
+                  <li>Wind: {props.data.wind}mph</li>
+                </ul>
+              </div>
+              <hr />
+              <div className="row">
+                <div className="col-2">
+                  <ul>
+                    <li>Thu</li>
+                    <li><img src="https://ssl.gstatic.com/onebox/weather/48/rain_light.png" alt="rain" /></li>
+                    <li>45</li>
+                  </ul>
+                </div>
+                <div className="col-2">
+                  <ul>
+                    <li>Fri</li>
+                    <li><img src="https://ssl.gstatic.com/onebox/weather/48/rain_light.png" alt="rain" /></li>
+                    <li>40</li>
+                  </ul>
+                </div>
+                <div className="col-2">
+                  <ul>
+                    <li>Sat</li>
+                    <li><img src="https://ssl.gstatic.com/onebox/weather/48/rain_light.png" alt="rain" /></li>
+                    <li>43</li>
+                  </ul>
+                </div>
+                <div className="col-2">
+                  <ul>
+                    <li>Sun</li>
+                    <li><img src="https://ssl.gstatic.com/onebox/weather/48/rain_light.png" alt="rain" /></li>
+                    <li>43</li>
+                  </ul>
+                </div>
+                <div className="col-2">
+                  <ul>
+                    <li>Mon</li>
+                    <li><img src="https://ssl.gstatic.com/onebox/weather/48/rain_light.png" alt="rain" /></li>
+                    <li>48</li>
+                  </ul>
+                </div>
+                <div className="col-2">
+                  <ul>
+                    <li>Tue</li>
+                    <li><img src="https://ssl.gstatic.com/onebox/weather/48/rain_light.png" alt="rain" /></li>
+                    <li>43</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+      
+ 
+    );
+}
