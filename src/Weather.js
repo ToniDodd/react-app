8b718cd9acff9af071706234eb8da7044622ccc8
ToniDodd <121724318+ToniDodd@users.noreply.github.com>
2023-02-02 16:42:43 -0800

    Added api call to update data

--- src/Weather.js
+++ src/Weather.js
@@ -1,21 +1,28 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 
 export default function Weather() {
+    const [ready, setReady] = useState(false);
+const [temp, setTemp] = useState(null);
+const [weatherData, setWeatherData] = useState({});
 
 function showData(response){
     console.log(response)
+    setTemp(Math.round(response.data.temperature.current));
+    setReady(true);
+    setWeatherData({
+        temperature: Math.round(response.data.temperature.current),
+        wind: Math.round(response.data.wind.speed),
+        humidity: response.data.temperature.humidity,
+        description: response.data.condition.description,
+        city: response.data.city
+
+    })
 }
 
-
-
-    let city = "Eugene";
-    let apiUrl= `https://api.shecodes.io/weather/v1/current?query=${city}&key=tb9021cb57677162636fa4a00f5o70a3&units=imperial`;
-axios.get(apiUrl).then(showData)
-
-  return (
-    
-      <div className="weather-app">
+if (ready){
+    return (
+<div className="weather-app">
         <form>
             <div className="row">
                 <div className="col-9">
@@ -24,20 +31,22 @@ axios.get(apiUrl).then(showData)
           <input type="submit" value="Search" className="city-submit" />
           </div>
           </div>
-          <h2 className="city">Eugene</h2>
+          <h2 className="city">{weatherData.city}</h2>
           <div classname="container">
             <div className="row">
               <div className="col-6">
                 <ul>
                   <li>Wednesday 3:10pm</li>
-                  <li>Clouds</li>
-                  <li><img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="partly cloudy" />46°F</li>
+                  <li>{weatherData.description}</li>
+                  <li><img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="partly cloudy" />
+                  <span className="temperature">{temp}</span>°<span>F
+                    </span></li>
                 </ul>
               </div>
               <div className="col-6">
                 <ul>
-                  <li>Precipitation: 45%</li>
-                  <li>Wind: 1 mph</li>
+                  <li>Humidity: {weatherData.humidity}%</li>
+                  <li>Wind:{weatherData.wind}mph</li>
                 </ul>
               </div>
               <hr />
@@ -91,5 +100,12 @@ axios.get(apiUrl).then(showData)
       
  
     </div>
-  );
+  ); 
+} else{
+    let city = "Eugene";
+    let apiUrl= `https://api.shecodes.io/weather/v1/current?query=${city}&key=tb9021cb57677162636fa4a00f5o70a3&units=imperial`;
+axios.get(apiUrl).then(showData)
+
+return("loading....")
+}
 }
